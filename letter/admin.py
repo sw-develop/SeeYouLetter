c5e6f9538da7b50ac3d4a35a5ab43200b3871398
sw-develop <anikapsw@gmail.com>
2020-12-25 02:04:41 +0900

    [FIX] register()

--- letter/admin.py
+++ letter/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Topic, Letter, Product, Content
 
-admin.site.register(Topic, Letter, Product, Content)
-
+admin.site.register(Topic)
+admin.site.register(Letter)
+admin.site.register(Product)
+admin.site.register(Content)
 
