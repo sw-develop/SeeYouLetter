ca87a0c019f72c08bb9236f19cf78d81f9fdd89f
sw-develop <anikapsw@gmail.com>
2020-12-25 01:55:28 +0900

    [ADD] apps

--- mysite/settings.py
+++ mysite/settings.py
@@ -55,6 +55,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'letter',
+    'photo',
+    'order',
 ]
 
 MIDDLEWARE = [
