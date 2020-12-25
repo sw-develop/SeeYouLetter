a8d1fec153cf2301e8bf15d138a4a0b31985a811
sw-develop <anikapsw@gmail.com>
2020-12-26 00:47:36 +0900

    [ADD] customer

--- order/admin.py
+++ order/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Order
+from .models import Customer, Order
 
 # Register your models here.
+admin.site.register(Customer)
 admin.site.register(Order)
