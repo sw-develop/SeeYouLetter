181d16d51818fb3577aff13d071b85fa779fbe9e
sw-develop <anikapsw@gmail.com>
2020-12-26 00:47:18 +0900

    [ADD] customer, order

--- order/models.py
+++ order/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from letter.models import Letter
 
-class Order(models.Model):
+class Customer(models.Model):
     letter = models.OneToOneField(Letter, on_delete=models.CASCADE, primary_key=True)
     sender = models.CharField(max_length=30)
     sender_phone_number = models.CharField(max_length=14)
-    #이메일
+    #이메일 -> 형식 체크 가능
     email = models.EmailField(
         verbose_name='email',
         max_length=255,
@@ -16,18 +16,30 @@ class Order(models.Model):
     receiver_addr = models.CharField(max_length=30)
     #우편방법
     MODE_OF_SEND_MAIL = [
-        ('regular mail', '일반우편'),
-        ('registered mail', '등기우편'),
+        ('regular_mail', '일반우편'),
+        ('registered_mail', '등기우편'),
     ]
     send_mail = models.CharField(
         max_length=20,
         choices=MODE_OF_SEND_MAIL,
-        default='regular mail',
+        default='regular_mail',
     )
 
     #개인정보동의
     #환불정책동의
-    #최종가격
 
+    class Meta:
+        db_table = 'customers'
+        verbose_name = 'Customer'
+        verbose_name_plural = 'Customers'
+
+class Order(models.Model):
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
+    price = models.IntegerField()
+
+    class Meta:
+        db_table = 'orders'
+        verbose_name = 'Order'
+        verbose_name_plural = 'Orders'
 
 
