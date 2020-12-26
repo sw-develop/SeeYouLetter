c3ad68f335f4ebafd4d15da079fe76e2dd5b90b7
sw-develop <anikapsw@gmail.com>
2020-12-26 22:08:33 +0900

    [ADD] meta class

--- photo/models.py
+++ photo/models.py
@@ -5,5 +5,9 @@ class Photo(models.Model):
     letter = models.ForeignKey(Letter, on_delete=models.CASCADE, related_name='user_photos')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d', default='photos/no_image.png')
 
+    class Meta:
+        db_table = 'photos'
+        verbose_name = 'Photo'
+        verbose_name_plural = 'Photos'
 
 
