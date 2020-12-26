c5582279ff5890817f42b55c13699f9959978c28
sw-develop <anikapsw@gmail.com>
2020-12-26 22:08:55 +0900

    [REMOVE] content

--- letter/admin.py
+++ letter/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Letter, Product, Content
+from .models import Letter, Product
 
 admin.site.register(Letter)
 admin.site.register(Product)
-admin.site.register(Content)
+
 
 
