6d70dae109b646f441ca48de88d0998635fe7c0d
sw-develop <anikapsw@gmail.com>
2020-12-26 23:52:57 +0900

    [ADD] Product APIView

--- letter/views.py
+++ letter/views.py
@@ -7,6 +7,50 @@ from rest_framework.response import Response
 from rest_framework import status
 
 # Create your views here.
+class ProductList(APIView):
+    """
+    List all Product or create a new Product
+    """
+    def get(self, request, format=None):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+class ProductDetail(APIView):
+    """
+    Retrieve, update or delete a Letter instance
+    """
+    def get_object(self, pk):
+        try:
+            return Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        product = self.get_object(pk)
+        serializer = LetterSerializer(product)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        product = self.get_object(pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 class LetterList(APIView):
     """
     List all Letter or create a new Letter
@@ -22,6 +66,7 @@ class LetterList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
 
 class LetterDetail(APIView):
     """
