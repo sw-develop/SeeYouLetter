cf7b60d3a62b5999e2a8703f3332537a29f1e994
sw-develop <anikapsw@gmail.com>
2020-12-27 15:15:40 +0900

    [ADD] User

--- letter/views.py
+++ letter/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .models import Product, Letter
-from .serializers import ProductSerializer, LetterSerializer
+from .models import Product, Letter, User
+from .serializers import ProductSerializer, LetterSerializer, UserSerializer
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -49,6 +49,51 @@ class ProductDetail(APIView):
     def delete(self, request, pk, format=None):
         product = self.get_object(pk)
         product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class UserList(APIView):
+    """
+    List all User or create a new User
+    """
+    def get(self, request, format=None):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+    #post 요청 시 cookie 설정 추가 해줘야함!
+    def post(self, request, format=None):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+class UserDetail(APIView):
+    """
+    Retrieve, update or delete a User instance
+    """
+    def get_object(self, pk):
+        try:
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        user = self.get_object(pk)
+        user.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 class LetterList(APIView):
