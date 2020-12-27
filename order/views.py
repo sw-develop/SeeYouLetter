7b2b3e4dc97a0097a44c88ce092bef97cda40ec7
sw-develop <anikapsw@gmail.com>
2020-12-27 14:14:17 +0900

    [ADD] customer, order view

--- order/views.py
+++ order/views.py
@@ -1,3 +1,68 @@
 from django.shortcuts import render
+from .models import Customer, Order
+from .serializers import CustomerSerializer, OrderSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+class CustomerList(APIView):
+    """
+    List all Customer or create a new Customer
+    """
+    def get(self, request, format=None):
+        customers = Customer.objects.all()
+        serializer = CustomerSerializer(customers, many=True)
+        return Response(serializer.data)
+
+    #수정할 것) post 요청 시 Order 객체 생성도 처리해줘야 함
+    def post(self, request, format=None):
+        serializer = CustomerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+class CustomerDetail(APIView):
+    """
+    Retrieve, update or delete a Customer instance
+    """
+    def get_object(self, pk):
+        try:
+            return Customer.objects.get(pk=pk)
+        except Customer.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        customer = self.get_object(pk)
+        serializer = CustomerSerializer(customer)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        customer = self.get_object(pk)
+        serializer = CustomerSerializer(customer, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        customer = self.get_object(pk)
+        customer.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class OrderDetail(APIView):
+    """
+    Retrieve, update or delete a Order instance
+    """
+    def get_object(self, pk):
+        try:
+            return Order.objects.get(pk=pk)
+        except Order.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        order = self.get_object(pk)
+        serializer = OrderSerializer(order)
+        return Response(serializer.data)
