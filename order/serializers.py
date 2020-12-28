97c96e03d2801d9523f834d1f274ab6ead5b3f12
sw-develop <anikapsw@gmail.com>
2020-12-28 22:11:33 +0900

    [REMOVE] nested relation

--- order/serializers.py
+++ order/serializers.py
@@ -3,14 +3,14 @@ from .models import Customer, Order
 from letter.serializers import LetterSerializer
 
 class CustomerSerializer(serializers.ModelSerializer):
-    letter = LetterSerializer(read_only=True)
+    #letter = LetterSerializer(read_only=True)
 
     class Meta:
         model = Customer
         fields = ['letter', 'senderName', 'senderPhone', 'sender_addr', 'receiver', 'receiver_phone_number', 'receiver_addr', 'send_mail']
 
 class OrderSerializer(serializers.ModelSerializer):
-    customer = CustomerSerializer(read_only=True)
+    #customer = CustomerSerializer(read_only=True)
 
     class Meta:
         model = Order
