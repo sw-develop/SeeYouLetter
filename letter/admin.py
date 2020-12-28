0c3d9199049c44c2f8931716366cf0554e6df5fc
sw-develop <anikapsw@gmail.com>
2020-12-28 21:27:58 +0900

    [ADD] add User

--- letter/admin.py
+++ letter/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Letter, Product
+from .models import Letter, Product, User
 
 admin.site.register(Letter)
 admin.site.register(Product)
-
+admin.site.register(User)
 
 
