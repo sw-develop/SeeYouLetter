a614d5e6fb74e29e9ca534ed97b79637a989cb46
sw-develop <anikapsw@gmail.com>
2020-12-28 15:31:46 +0900

    [ADD] Customer field

--- order/models.py
+++ order/models.py
@@ -3,6 +3,9 @@ from letter.models import Letter
 
 class Customer(models.Model):
     letter = models.OneToOneField(Letter, on_delete=models.CASCADE, primary_key=True)
+    senderName = models.CharField(max_length=30)
+    senderPhone = models.CharField(max_length=14)
+    sender_addr = models.CharField(max_length=30)  # 우편번호랑 상세주소 다같이 저장되는건가?
     receiver = models.CharField(max_length=30)
     receiver_phone_number = models.CharField(max_length=14)
     receiver_addr = models.CharField(max_length=30)
