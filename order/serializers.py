63a2271caef1b5f226c0b8292260dd3852401c85
sw-develop <anikapsw@gmail.com>
2020-12-28 20:43:33 +0900

    [ADD] nested relationship

--- order/serializers.py
+++ order/serializers.py
@@ -1,13 +1,17 @@
 from rest_framework import serializers
 from .models import Customer, Order
-from letter.models import Letter
+from letter.serializers import LetterSerializer
 
 class CustomerSerializer(serializers.ModelSerializer):
+    letter = LetterSerializer(read_only=True)
+
     class Meta:
         model = Customer
-        fields = ['letter']
+        fields = ['letter', 'senderName', 'senderPhone', 'sender_addr', 'receiver', 'receiver_phone_number', 'receiver_addr', 'send_mail']
 
 class OrderSerializer(serializers.ModelSerializer):
+    customer = CustomerSerializer(read_only=True)
+
     class Meta:
         model = Order
         fields = ['customer', 'price']
