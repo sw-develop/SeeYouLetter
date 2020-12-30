6bba4f95b7953dd33f8c551b3c6c301383827309
sw-develop <anikapsw@gmail.com>
2020-12-30 17:08:32 +0900

    [ADD] test localhost

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,7 +75,8 @@ MIDDLEWARE = [
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3001',
-    'http://localhost:3002'
+    'http://localhost:3002',
+    'http://localhost:3000'
 ]
 
 ROOT_URLCONF = 'mysite.urls'
