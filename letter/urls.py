d8ee60901ea537060a408aaf695d6cf9c43c582a
sw-develop <anikapsw@gmail.com>
2021-01-01 23:17:01 +0900

    [ADD] Topic

--- letter/urls.py
+++ letter/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     path('users/<int:pk>/', views.UserDetail.as_view()),
     path('letters/', views.LetterList.as_view()),
     path('letters/<int:pk>/', views.LetterDetail.as_view()),
+    path('topics/', views.TopicList.as_view()),
+    path('topics/<int:pk>/', views.TopicDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
