0a44a17718a99fcf59f815de40db5913c7b30646
sw-develop <anikapsw@gmail.com>
2021-01-01 23:03:47 +0900

    [ADD] Topic

--- letter/admin.py
+++ letter/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Letter, Product, User
+from .models import Letter, Product, User, Topic
 
 admin.site.register(Letter)
 admin.site.register(Product)
 admin.site.register(User)
-
+admin.site.register(Topic)
 
