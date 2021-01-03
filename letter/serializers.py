b193214dc5f76bdfc613c1d440486a0914a39c94
sw-develop <psw00719@naver.com>
2021-01-03 19:41:21 +0900

    [ADD] Letter field

--- letter/serializers.py
+++ letter/serializers.py
@@ -12,11 +12,11 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'senderEmail']
 
 class LetterSerializer(serializers.ModelSerializer):
-    products = ProductSerializer(read_only=True)
+    #product = ProductSerializer(read_only=True)
 
     class Meta:
         model = Letter
-        fields = ['id', 'products', 'user', 'topics', 'date', 'letter_content', 'page', 'photo_price']
+        fields = ['id', 'paper', 'user', 'SelectedQuestions', 'font', 'date', 'letter_content', 'page', 'photo_price']
 
 class TopicSerializer(serializers.ModelSerializer):
     class Meta:
