43c763c7582164433d7e5ab7892fb68aa325b16c
sw-develop <psw00719@naver.com>
2021-01-05 00:55:45 +0900

    [ADD] create()

--- letter/serializers.py
+++ letter/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Product, Letter, User, Topic
+from django.db import transaction
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
@@ -18,8 +19,28 @@ class TopicSerializer(serializers.ModelSerializer):
 
 class LetterSerializer(serializers.ModelSerializer):
     #product = ProductSerializer(read_only=True)
-    #SelectedQuestions = TopicSerializer(read_only=True)
+    SelectedQuestions = TopicSerializer(many=True)
+
     class Meta:
         model = Letter
         fields = ['id', 'paper', 'user', 'SelectedQuestions', 'font', 'month', 'letter_content', 'page', 'photo_price']
+
+        @transaction.atomic
+        def create(self, validated_data):
+            topics_data = validated_data.pop('SelectedQuestions')
+            letter = super(LetterSerializer, self).create(validated_data)
+
+            #Validates topics data and creates SelectedQuestions
+            try:
+                topic_objects = TopicSerializer(data=topics_data, many=True)
+                topic_objects.is_valid(raise_exception=True)
+                topics = topic_objects.save()
+                letter.SelectedQuestions.add(topics) #add topics to letter
+            except:
+                raise serializers.ValidationError("topics not valid.")
+
+            letter.save()
+
+            return letter
+
 
