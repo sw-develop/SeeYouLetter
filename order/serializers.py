98276005d25a1851ba1d6294e04c2d7fe52ce268
sw-develop <psw00719@naver.com>
2021-01-05 16:15:25 +0900

    [FIX] fields

--- order/serializers.py
+++ order/serializers.py
@@ -1,18 +1,15 @@
 from rest_framework import serializers
 from .models import Customer, Order
-from letter.serializers import LetterSerializer
 
 class CustomerSerializer(serializers.ModelSerializer):
-    #letter = LetterSerializer(read_only=True)
 
     class Meta:
         model = Customer
-        fields = ['letter', 'senderName', 'senderPhone', 'senderAddr', 'receiver', 'receiverPhone', 'receiverAddr', 'send_mail']
+        fields = ['letter', 'senderName', 'senderPhone', 'senderFullAddress', 'senderDetailedAddress', 'senderZoneCode', 'receiver', 'receiverPhone', 'receiverFullAddress', 'receiverDetailedAddress', 'receiverZoneCode', 'postMethod']
 
 class OrderSerializer(serializers.ModelSerializer):
-    #customer = CustomerSerializer(read_only=True)
 
     class Meta:
         model = Order
-        fields = ['customer', 'letterName', 'letterPrice', 'letterPage_count', 'photo_price', 'send_mail', 'total_price', 'created_date']
+        fields = ['customer', 'letterName', 'letterPrice', 'letterPage_count', 'photo_price', 'postMethod', 'total_price', 'created_date']
 
