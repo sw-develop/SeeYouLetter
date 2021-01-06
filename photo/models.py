914f16853c30ea44ae397885f1efe46a3ba594d5
sw-develop <psw00719@naver.com>
2021-01-07 02:19:05 +0900

    [ADD] image_url

--- photo/models.py
+++ photo/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from letter.models import Letter
 import os
+from django.core.files import File
+from urllib.request import urlopen
+from tempfile import NamedTemporaryFile
 
 from mysite import settings
 
@@ -8,15 +11,25 @@ from mysite import settings
 class Photo(models.Model):
     letter = models.ForeignKey(Letter, on_delete=models.CASCADE, related_name='user_photos')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d', default='photos/no_image.png')
+    image_url = models.URLField(null=True)
 
     def delete(self, *args, **kargs):
         if self.photo:
             os.remove(os.path.join(settings.MEDIA_ROOT, self.photo.path))
         super(Photo, self).delete(*args, **kargs)
 
+    def get_remote_image(self):
+        if self.image_url and not self.photo:
+            img_temp = NamedTemporaryFile(delete=True)
+            img_temp.write(urlopen(self.image_url).read())
+            img_temp.flush()
+            self.photo.save(f"image_{self.pk}", File(img_temp))
+        self.save()
+
     class Meta:
         db_table = 'photos'
         verbose_name = 'Photo'
         verbose_name_plural = 'Photos'
+        get_latest_by = 'pk'
 
 
