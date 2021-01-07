af8f2e8b002188d048b9c006d825598babfd1209
sw-develop <psw00719@naver.com>
2021-01-07 23:23:55 +0900

    [FIX] Letter put

--- letter/views.py
+++ letter/views.py
@@ -173,7 +173,7 @@ class LetterDetail(APIView):
         serializer = LetterSerializer(letter, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk, format=None):
