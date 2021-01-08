761745797017212c2f167697c24117eceabd8250
sw-develop <psw00719@naver.com>
2021-01-09 00:39:09 +0900

    [REMOVE] about media file

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,12 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('letter/', include('letter.urls')),
     path('order/', include('order.urls')),
     path('photo/', include('photo.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
