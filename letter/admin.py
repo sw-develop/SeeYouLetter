3841c28b1b54026039818dcdaa2f9048bd6cd93d
sw-develop <psw00719@naver.com>
2021-01-08 17:51:36 +0900

    [ADD] modelAdmin

--- letter/admin.py
+++ letter/admin.py
@@ -1,8 +1,32 @@
 from django.contrib import admin
 from .models import Letter, Product, User, Topic
 
-admin.site.register(Letter)
-admin.site.register(Product)
 admin.site.register(User)
 admin.site.register(Topic)
+@admin.register(Letter)
+class LetterAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'paper',
+        'user',
+        'font',
+        'month',
+        'page',
+        'photo_price'
+    )
+    list_display_links = (
+        'paper',
+        'user'
+    )
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'id'
+        'name',
+        'price'
+    )
+
+
+
 
